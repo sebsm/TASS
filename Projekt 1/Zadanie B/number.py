8b3624b52325cf386f981c9ccea7e71d0a542ebd
selvert <01102827@pw.edu.pl>
2020-11-22 23:28:38 +0100

    K-cores v2

--- Projekt 1/Zadanie B/number.py
+++ Projekt 1/Zadanie B/number.py
@@ -1,19 +1,22 @@
 def number(num):
     return num % 6
 
+def number2(num):
+    return num % 7
+
 print(number(269056))
+print(number2(269056))
 
+# def compare(arr1, arr2):
+#     sum = 0
+#     for i in arr1:
+#         if i in arr2:
+#             sum +=1
+#         else:
+#             continue
+#     return sum
 
-def compare(arr1, arr2):
-    sum = 0
-    for i in arr1:
-        if i in arr2:
-            sum +=1
-        else:
-            continue
-    return sum
+# a1 = [1,2,3,4,5,6]
+# a2 = [2,3,4,5,6]
 
-a1 = [1,2,3,4,5,6]
-a2 = [2,3,4,5,6]
-
-print(compare(a1,a2))
+# print(compare(a1,a2))
